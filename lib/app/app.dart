090905ac303880109aa8d3b153f1e6e2a978bd31
christinevanesyan <127954501+christinevanesyan@users.noreply.github.com>
2025-06-15 21:47:30 +0400

    WIP

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:game/screens/onboarding_screen/onboarding_screen.dart';
+import 'package:game/screens/preloader_screen/preloader_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -15,7 +15,7 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const OnboardingScreen(),
+      home: PreloaderScreen(),
     );
   }
 }
